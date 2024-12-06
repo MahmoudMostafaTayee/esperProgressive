1d5a7cb8bd1539b4588d65dfb133b526f92be8f7
MMTayee <mahmoud.tayee@thirdwayv.com>
2024-12-06 19:40:02 +0200

    Some enhancements and cleaning.

--- src/main/java/com/espertech/esper/example/IOT/EventEPLUtil.java
+++ src/main/java/com/espertech/esper/example/IOT/EventEPLUtil.java
@@ -6,10 +6,7 @@ import com.espertech.esper.common.client.EPCompiled;
 import com.espertech.esper.common.client.configuration.Configuration;
 import com.espertech.esper.common.client.util.NameAccessModifier;
 import com.espertech.esper.compiler.client.CompilerArguments;
-import com.espertech.esper.compiler.client.EPCompileException;
 import com.espertech.esper.compiler.client.EPCompilerProvider;
-import com.espertech.esper.runtime.client.DeploymentOptions;
-import com.espertech.esper.runtime.client.EPDeployException;
 import com.espertech.esper.runtime.client.EPDeployment;
 import com.espertech.esper.runtime.client.EPRuntime;
 import com.espertech.esper.runtime.client.EPStatement;
@@ -21,17 +18,6 @@ import org.slf4j.LoggerFactory;
 public class EventEPLUtil {
     private static final Logger log = LoggerFactory.getLogger(EventEPLUtil.class);
 
-    public static EPCompiled compileEPL(Configuration configuration, String eplQuery) {
-        log.info("Compiling EPL");
-        try {
-            EPCompiled compiled = EPCompilerProvider.getCompiler().compile(eplQuery, new CompilerArguments(configuration));
-
-            return compiled;
-        } catch (EPCompileException ex) {
-            throw new RuntimeException("Failed to compile EPL", ex);
-        }
-    }
-
     public static Configuration getConfiguration() {
         Configuration configuration = new Configuration();
         configuration.getCommon().addEventType("sensorData", SensorData.class);
@@ -39,15 +25,13 @@ public class EventEPLUtil {
         return configuration;
     }
 
-    public static void deploy(EPRuntime runtime, EPCompiled compiled) {
-        try {
-            runtime.getDeploymentService().deploy(compiled);
-        } catch (EPDeployException ex) {
-            throw new RuntimeException("Failed to deploy EPL", ex);
-        }
+    public static void compileDeployAddListener(EPRuntime runtime, String eplQuery, UpdateListener listener){
+        EPStatement statement;
+        statement = EventEPLUtil.compileDeploy(runtime, eplQuery);
+        EventEPLUtil.add_listener(statement, listener);
     }
 
-    public static EPStatement compileDeploy(EPRuntime runtime, String epl) {
+    private static EPStatement compileDeploy(EPRuntime runtime, String epl) {
         try {
             CompilerArguments args = new CompilerArguments();
             args.getPath().add(runtime.getRuntimePath());
@@ -61,7 +45,7 @@ public class EventEPLUtil {
         }
     }
 
-    public static void add_listener(EPStatement statement, UpdateListener listener){
+    private static void add_listener(EPStatement statement, UpdateListener listener){
         // EPStatement statement = runtime.getDeploymentService().getStatement(deploymentId, eplQuery_name);
         if (statement != null) {
             statement.addListener(listener);
--- src/main/java/com/espertech/esper/example/IOT/IotMain.java
+++ src/main/java/com/espertech/esper/example/IOT/IotMain.java
@@ -1,32 +1,12 @@
-/*
- ***************************************************************************************
- *  Copyright (C) 2006 EsperTech, Inc. All rights reserved.                            *
- *  http://www.espertech.com/esper                                                     *
- *  http://www.espertech.com                                                           *
- *  ---------------------------------------------------------------------------------- *
- *  The software in this package is published under the terms of the GPL license       *
- *  a copy of which has been included with this distribution in the license.txt file.  *
- ***************************************************************************************
- */
 package com.espertech.esper.example.IOT;
 
-import com.espertech.esper.common.client.EPCompiled;
 import com.espertech.esper.common.client.configuration.Configuration;
-import com.espertech.esper.common.client.util.NameAccessModifier;
-import com.espertech.esper.compiler.client.CompilerArguments;
-import com.espertech.esper.compiler.client.EPCompilerProvider;
-import com.espertech.esper.runtime.client.DeploymentOptions;
-import com.espertech.esper.runtime.client.EPDeployException;
-import com.espertech.esper.runtime.client.EPDeployment;
 import com.espertech.esper.runtime.client.EPRuntime;
 import com.espertech.esper.runtime.client.EPRuntimeProvider;
-import com.espertech.esper.runtime.client.EPStatement;
 import com.espertech.esper.runtime.client.UpdateListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.LinkedList;
 
 public class IotMain implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(IotMain.class);
@@ -56,18 +36,25 @@ public class IotMain implements Runnable {
         generator.generateEvents(runtime);
     }
 
+    private static void compileDeployAddListener(String eplQuery, UpdateListener listener){
+        EventEPLUtil.compileDeployAddListener(  runtime, 
+                                                eplQuery, 
+                                                listener);
+    }
+
     public void run() {
         initiateRunTime();
         String eplQuery;
-        EPStatement statement;
 
         eplQuery = "select * from sensorData output all every 4 seconds order by timestamp;";
         // String eplQuery = "@name('out') select count(*) as count_num, sum(value) as total from sensorData output last every 2 seconds;";
         // String eplQuery = "@name('out') select count(*) as count_num, sum(value) as total from sensorData#time(4);";
         // String eplQuery = "@name('out') select count(*) as count_num, sum(value) as total from sensorData#time(5);";
-        statement = EventEPLUtil.compileDeploy(runtime, eplQuery);
-        EventEPLUtil.add_listener(statement, new GenericIotEventListener("Out sensorData every 4 seconds Event"));
-        
+        compileDeployAddListener(   
+                                    eplQuery, 
+                                    new GenericIotEventListener("Out sensorData every 4 seconds Event")
+                                );
+
         eplQuery = "insert into CombinedEvent(deviceId, type, command, value, timestamp)" +
         "select D.deviceId," +
         "type," +
@@ -76,11 +63,13 @@ public class IotMain implements Runnable {
         "D.timestamp " +
         "from sensorData#time(5 sec) D," +
         "deviceCommand#time(5 sec) C " +
-        "where D.deviceId = C.deviceId";
+        "where D.deviceId = C.deviceId;";
         
         // eplQuery = "select * from deviceCommand;";
-        statement = EventEPLUtil.compileDeploy(runtime, eplQuery);
-        EventEPLUtil.add_listener(statement, new GenericIotEventListener("Combined event"));
+        compileDeployAddListener(   
+                                    eplQuery, 
+                                    new GenericIotEventListener("Combined event")
+                                );
 
         add_generator(new IotStreamGenerator());
         
