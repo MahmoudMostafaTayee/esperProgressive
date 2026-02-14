1e18349a32c4519e485cd15c9ed54ca8c2f70088
MahmoudMostafaTayee <mahmoudmostafatayee@outlook.com>
2026-02-14 10:40:59 +0200

    start the listener with the server.

--- run_all_cameras.bat
+++ run_all_cameras.bat
@@ -2,4 +2,8 @@
 REM Run script for Esper IOT Example with ALL cameras
 set MAIN_CLASS=com.espertech.esper.example.IOT.IotMain
 set ARGS=--scene 1 --features_dir C:\OURs\Thesis\Real-Time_Multi-Camera_People_Tracking_using_Event_Stream_Processing\Datasets\EmbedFeature --camera all --output_dir ./output/scene1
+
+echo Starting Python Table Listener in a new window...
+start "Python Table Listener" cmd /c "python listen_to_table.py"
+
 mvn exec:java -Dexec.mainClass="%MAIN_CLASS%" -Dexec.args="%ARGS%" -Dcheckstyle.skip
