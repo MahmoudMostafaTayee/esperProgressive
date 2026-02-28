bee8b86d6abcec8482fc501e2bb355805654f63b
MahmoudMostafaTayee <mahmoudmostafatayee@outlook.com>
2026-02-28 07:47:12 +0200

    free port before start

--- run_all_cameras.bat
+++ run_all_cameras.bat
@@ -1,5 +1,8 @@
 @echo off
 REM Run script for Esper IOT Example with ALL cameras
+
+echo Cleaning up port 9999...
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr :9999 ^| findstr LISTENING') do taskkill /f /pid %%a
 set MAIN_CLASS=com.espertech.esper.example.IOT.IotMain
 set ARGS=--scene 1 --features_dir C:\OURs\Thesis\Real-Time_Multi-Camera_People_Tracking_using_Event_Stream_Processing\Datasets\EmbedFeature --camera all --output_dir ./output/scene1
 
